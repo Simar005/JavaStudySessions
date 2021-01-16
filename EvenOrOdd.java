4f851891bf3fab2c07876e783bc4fa66e48971a3
Simar005 <letslive818@gmail.com>
2021-01-15 22:10:55 -0600

    first commit

---
+++ EvenOrOdd.java
@@ -0,0 +1,54 @@
+package squad;
+
+import java.util.Scanner;
+
+//practice interview question!
+//why is this message not able to push to git hub
+public class EvenOrOdd {
+	
+	
+	Scanner scan=new Scanner(System.in);
+	
+	public void EvOrOd(int a) {
+		if (  a % 2 == 0  ) {
+			
+			System.out.println( " its an even number ");
+					
+					
+			}
+			
+			else if ( a % 2 == 1) { 
+				
+				System.out.println( " it's an Odd number ");
+				
+			}
+			
+			else {
+				System.out.println(" None of them ");
+			}
+	}
+	
+	
+	public void  EvenOrOdd() {
+		System.out.println("Enter a number: ");
+		int num = scan.nextInt();
+		
+		if (  num % 2 == 0  ) {
+			
+		System.out.println( " its an even number ");
+				
+				
+		}
+		
+		else if ( num % 2 == 1) { 
+			
+			System.out.println( " it's an Odd number ");
+			
+		}
+		
+		else {
+			System.out.println(" None of them ");
+		}
+	}
+
+}
